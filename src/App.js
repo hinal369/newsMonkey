8abb75ed20fdfea157a8381c9e16ce51933ce577
Hinal Khalasi <khalasihinal1998@gmail.com>
2023-09-05 13:53:23 +0530

    Env

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 import Navbar from './components/Navbar';
 import News from './components/News';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 export default class App extends Component {
+  apiKey = process.env.REACT_APP_API_KEY
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
           <Routes>
-            <Route index element={<News key="general" pageSize={9} country='in' apiKey="400b694a61bf4f39854157d3cfa423e6" category='general'/>} />
-            <Route path="business" element={<News key="business" pageSize={9} country='in' apiKey="400b694a61bf4f39854157d3cfa423e6" category='business'/>} />
-            <Route path="entertainment" element={<News key="entertainment" pageSize={9} country='in' apiKey="400b694a61bf4f39854157d3cfa423e6" category='entertainment'/>} />
-            <Route path="health" element={<News key="health" pageSize={9} country='in' apiKey="400b694a61bf4f39854157d3cfa423e6" category='health'/>} />
-            <Route path="science" element={<News key="science" pageSize={9} country='in' apiKey="400b694a61bf4f39854157d3cfa423e6" category='science'/>} />
-            <Route path="sports" element={<News key="sports" pageSize={9} country='in' apiKey="400b694a61bf4f39854157d3cfa423e6" category='sports'/>} />
-            <Route path="technology" element={<News key="technology" pageSize={9} country='in' apiKey="400b694a61bf4f39854157d3cfa423e6" category='technology'/>} />
+            <Route index element={<News key="general" pageSize={9} country='in' apiKey={this.apiKey} category='general'/>} />
+            <Route path="business" element={<News key="business" pageSize={9} country='in' apiKey={this.apiKey} category='business'/>} />
+            <Route path="entertainment" element={<News key="entertainment" pageSize={9} country='in' apiKey={this.apiKey} category='entertainment'/>} />
+            <Route path="health" element={<News key="health" pageSize={9} country='in' apiKey={this.apiKey} category='health'/>} />
+            <Route path="science" element={<News key="science" pageSize={9} country='in' apiKey={this.apiKey} category='science'/>} />
+            <Route path="sports" element={<News key="sports" pageSize={9} country='in' apiKey={this.apiKey} category='sports'/>} />
+            <Route path="technology" element={<News key="technology" pageSize={9} country='in' apiKey={this.apiKey} category='technology'/>} />
           </Routes> 
         </div>
       </BrowserRouter>
